1ddb968cef3eed7ee5a839838034a1a26069ff6f
Aina Rakotomalala <kantoniaina@hotmail.com>
2026-03-26 14:54:20 +0100

    Réaménagement section A propos en blocs + Ajout animation photo Intro et réglage responsive

--- script.js
+++ script.js
@@ -123,3 +123,31 @@ links.forEach((link) => {
     overlay.classList.remove("active");
   });
 });
+
+// animation cartes A propos
+
+const cards = document.querySelectorAll(".about-block");
+
+cards.forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--x", `${x}px`);
+    card.style.setProperty("--y", `${y}px`);
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 14;
+    const rotateY = (centerX - x) / 14;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.04)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0) scale(1)";
+  });
+});
